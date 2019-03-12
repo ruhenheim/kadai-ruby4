dfd0b6e448ec533ca0a3dc093b8f3f65d498e756
ruhenheim <asd.uchiyama@gmail.com>
2019-03-12 09:47:29 +0000

    bundler/inline利用、sinatra併用時対処、及び外部公開用の設定

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
-require 'sinatra'
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem "sinatra"
+  gem "puma"
+end
+
+set :app_file, $0   # https://github.com/sinatra/sinatra/issues/1507
+
+set :bind, '0.0.0.0'
+set :port, 8080
 
 get '/' do
   erb :contact
